6fbdf1caa181155016785a170a1a18cc9a87a895
kyxnguk <ghdruddnr1@gmail.com>
2024-11-27 23:01:56 +0900

    skco path

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./common/Layout";
+import LocationSelect from "./pages/LocationSelect";
+import TransportSelect from "./pages/TransportSelect";
+import Result from "./pages/Result";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/location" element={<LocationSelect />} />
+          <Route path="/transport" element={<TransportSelect />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
